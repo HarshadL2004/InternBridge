b22739f422e9167e1c52785a2dc0a16ae0d57bb9
HarshadL2004 <harshadlohakare0412@gmail.com>
2026-04-23 23:42:27 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -44,7 +44,7 @@ const Home = () => {
         const fetchJobs = async () => {
             try {
                 setLoading(true);
-                const localRes = await fetch("https://internbridge-backend-p5xk.onrender.com/internships");
+                const localRes = await fetch("https://internbridge-backend-098c.onrender.com");
                 const localData = await localRes.json();
 
                 const arbeitRes = await fetch("https://www.arbeitnow.com/api/job-board-api");
