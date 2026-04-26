9f82d632756876ef3c050054ec3f4c8cdd4923a3
HarshadL2004 <harshadlohakare0412@gmail.com>
2026-04-26 09:23:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 5001;
 
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
 
 // mongodb connection 
 
